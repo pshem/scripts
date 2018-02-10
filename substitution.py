e3dd146314937454dfac298eeb301c5a537bf2f6
pshem <ppikula@rocketmail.com>
2018-02-10 18:01:43 +0000

    Case insensitive parsing with the -c flag added

--- substitution.py
+++ substitution.py
@@ -10,19 +10,24 @@ if len(sys.argv) == 1:
 	raise ValueError('No file passed to decode')
 elif len(sys.argv) > 3:
 	raise ValueError('Too many arguments passed. Pass just one')
-elif len(sys.argv) == 2:
-	if sys.argv[2] == '-c':
-		#case insensitive
-	else:
+elif len(sys.argv) == 3:
+	if (sys.argv[2] != '-c'):
 		raise ValueError('Use -c if you want case insensitive parsing')
 
 ciphertext = []
+
+#select the characters you want to deal with
+important_chars = string.ascii_lowercase + string.ascii_uppercase
 
 with open(sys.argv[1], 'r') as encryptedFile:
 	for line in encryptedFile:
 		#remove trailing newlines
 		while line.endswith(chr(10)): #'/n' newline
-			line = line[:-1]
+			if (sys.argv[2] == '-c'):
+				line = line[:-1].lower()
+				important_chars = string.ascii_lowercase
+			else:
+				line = line[:-1]
 		ciphertext.append(line)
 		print(ciphertext[-1])
 
@@ -34,8 +39,7 @@ for i in ciphertext:
 
 #print(letter_frequency) #would also print characters we don't want
 
-#select the characters you want to deal with
-important_chars = string.ascii_lowercase + string.ascii_uppercase
+
 
 for letter in important_chars:
     print(letter, " : ", letter_frequency[letter])
