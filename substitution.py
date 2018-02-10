c1670b55d7406de6b55015273379e8359fc5ba4c
pshem <ppikula@rocketmail.com>
2018-02-10 19:48:45 +0000

    Nice letter frequnecy format

--- substitution.py
+++ substitution.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-#
+
+#By @pshem and @ZsanettM 2018, Licensed Apache 2.0
+#tested in Python 3.5
 
 import sys
 import string
@@ -37,9 +39,26 @@ for i in ciphertext:
 	freq = collections.Counter(i)
 	letter_frequency += freq
 
-#print(letter_frequency) #would also print characters we don't want
+#get the number of letters
+numLetters = 0
+for letter in important_chars:
+	numLetters += letter_frequency[letter]
+
+englishFrequency = dict(a=8.167, b=1.492, c=2.782, d=4.253, e=12.702, f=2.228,
+ g=2.015, h=6.094, i=6.966, j=0.153, k=0.772, l=4.025, m=2.406, n=6.749,
+  o=7.507, p=1.929, q=0.095, r=5.987, s=6.327, t=9.056, u=2.758, v=0.978,
+   w=2.360, x=0.150, y=1.974, z=0.074)
 
 
+fmt = "{letter:s}\t{FoundInText:d}\t{FrequencyInText:0.2f}%\t{FrequencyInEnglish:0.2f}%"
+head = "{letter:s}\t{FoundInText:s}\t{FrequencyInText:s}\t{FrequencyInEnglish:s}"
+#print('Letter \t','Times \t', '% of Text \t', '% of English')
+
+print(head.format(letter = 'Letter', FoundInText = 'Times', FrequencyInText = 'Freq',FrequencyInEnglish = 'Freq'))
+print(head.format(letter = '', FoundInText = 'in', FrequencyInText = 'in',FrequencyInEnglish = 'in'))
+print(head.format(letter = '', FoundInText = 'text', FrequencyInText = 'text',FrequencyInEnglish = 'English'))
 
 for letter in important_chars:
-    print(letter, " : ", letter_frequency[letter])
+    print(fmt.format(letter = letter, FoundInText = letter_frequency[letter],
+	 FrequencyInText = ((letter_frequency[letter]/numLetters)*100),
+	 FrequencyInEnglish = englishFrequency[letter])) #if english letters
