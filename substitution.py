d6f9788f13562385c0486e8dfd3d33894fc40843
pshem <ppikula@rocketmail.com>
2018-02-12 16:07:36 +0000

    Now analyses same letter sequences

--- substitution.py
+++ substitution.py
@@ -50,9 +50,10 @@ for i in ciphertext:
 
 #get all letters which appear next to themselves, like 'm' in common
 di_letter_freq = collections.Counter()
-#for i in ciphertext:
-#TODO: Implement
-
+for i in ciphertext:
+	for j, k in zip(i, i[1:]):
+		if j == k:
+			di_letter_freq[j] +=1
 
 word_freq = collections.Counter()
 for i in ciphertext:
@@ -72,6 +73,7 @@ engLetterFreq = collections.defaultdict(lambda:0, a=8.167, b=1.492, c=2.782,
  d=4.253, e=12.702, f=2.228, g=2.015, h=6.094, i=6.966, j=0.153, k=0.772,
  l=4.025, m=2.406, n=6.749, o=7.507, p=1.929, q=0.095, r=5.987, s=6.327,
  t=9.056, u=2.758, v=0.978, w=2.360, x=0.150, y=1.974, z=0.074)
+engDiLetterList = frozenset(["ll", "ee", "ss", "oo", "tt", "ff", "rr", "nn", "pp", "cc"])
  #statistics from https://en.wikipedia.org/wiki/Most_common_words_in_English
  # ~100 most common english words. All forms of be added(is, are)
 engWordList1 = frozenset(["a", "I"])
@@ -84,16 +86,19 @@ engWordList4 = frozenset(["that", "have", "with", "this", "from", "they", "will"
 #format the frequency table roughly with tabs
 #TODO:redo with https://docs.python.org/3.5/library/string.html#format-specification-mini-language
 head = "{a:s}\t{b:s}\t{c:s}\t{d:s}"
+head2 = "{a:s}\t{b:s}\t{c:s}\t{d:s}\t\t{e:s}\t{f:s}\t{f:s}"
 fmt = "{a:s}\t{b:d}\t{c:0.2f}%\t{d:0.2f}%"
+fmt2 = "{a:s}\t{b:d}\t{c:0.2f}%\t{d:0.2f}%\t\t{e:s}\t{f:d}\t{g:b}"
 
-print(head.format(a = 'Letter', b = 'Times', c = 'Freq', d = 'Freq'))
-print(head.format(a = '', b = 'in', c = 'in', d = 'in'))
-print(head.format(a = '', b = 'text', c = 'text', d = 'English'))
+print(head2.format(a = 'Letter', b = 'Times', c = 'Freq', d = 'Freq', e = 'Letter', f = 'Is', g = 'Is'))
+print(head2.format(a = '', b = 'in', c = 'in', d = 'in', e = "Sequ-", f = 'in', g = 'in'))
+print(head2.format(a = '', b = 'text', c = 'text', d = 'English', e = 'ence', f = 'Text', g = 'English'))
 
 for letter in important_chars:
-    print(fmt.format(a = letter, b = letter_freq[letter],
-	 c = ((letter_freq[letter]/numLetters)*100),
-	 d = engLetterFreq[letter])) #if english letters
+    print(fmt2.format(a = letter, b = letter_freq[letter],
+	 c = (letter_freq[letter]/numLetters)*100,
+	 d = engLetterFreq[letter], e = letter+letter, f = bool(di_letter_freq[letter]),
+ 	 g = bool(letter+letter in engDiLetterList))) #if english letters
 
 #word frequency table
 #TODO: strip the '-' word
