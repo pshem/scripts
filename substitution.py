e864254dce22b2c341f92b81137374f40c8d9e62
pshem <ppikula@rocketmail.com>
2018-06-01 21:01:47 +0100

    Make getFrequencies self contained, fix head2 format

--- substitution.py
+++ substitution.py
@@ -42,13 +42,14 @@ def parseCiphertext(infile = args.infile, case = args.case, verbose = args.verbo
 
 ciphertext = parseCiphertext()
 
-#TODO: check the performance with ciphertext as a globa/local variable
-def getFrequencies():
+#TODO: performance optimisations
+def getFrequencies(current_ciphertext):
 	letter_freq = collections.Counter()
 	di_letter_freq = collections.Counter()
 	word_freq = collections.Counter()
 
-	for i in ciphertext:
+	#letter frequency table
+	for i in current_ciphertext:
 		#get the frequency at which letters are present in text
 		freq = collections.Counter(i)
 		letter_freq += freq
@@ -60,13 +61,39 @@ def getFrequencies():
 		words = i.split()
 		freq = collections.Counter(words)
 		word_freq += freq
-	return list([letter_freq, di_letter_freq, word_freq])
 
-#For now, split back into 3 Counters
-quantities = getFrequencies()
+	#word frequency table
+	#TODO: strip the '-' word
+	unique_words = word_freq.most_common()		#turn the Counter into a list
+	one_letter_word_freq = list()				#create separate lists for one,
+	two_letter_word_freq = list()				#two and three letter words
+	tri_letter_word_freq = list()				#and longer ones, but only if they
+	long_word_freq = list()						#appear multiple times
+	#<list>[x][0] gives the key, <list>[x][1] gives the number
+	numWords = len(unique_words) #may containg junk such as -
+
+	for i in unique_words:
+		#print(i[0], i[1])	#for debugging
+		if len(i[0]) == 1:
+			one_letter_word_freq.append(i)
+		elif len(i[0]) == 2:
+			two_letter_word_freq.append(i)
+		elif len(i[0]) == 3:
+			tri_letter_word_freq.append(i)
+		elif i[1] > 1:
+			long_word_freq.append(i)
+	return list([letter_freq, di_letter_freq, one_letter_word_freq, two_letter_word_freq, tri_letter_word_freq, long_word_freq, numWords])
+#end getFrequencies()
+
+#For now, split back into 6 Counters and an integer
+quantities = getFrequencies(ciphertext)
 letter_freq = quantities[0]
 di_letter_freq = quantities[1]
-word_freq = quantities[2]
+one_letter_word_freq = quantities[2]
+two_letter_word_freq = quantities[3]
+tri_letter_word_freq = quantities[4]
+long_word_freq = quantities[5]
+numWords = quantities[6]
 
 #get the total number of letters
 numLetters = 0
@@ -85,14 +112,12 @@ engDiLetterList = frozenset(["ll", "ee", "ss", "oo", "tt", "ff", "rr", "nn", "pp
 engWordList1 = frozenset(["a", "I"])
 engWordList2 = frozenset(["be", "to", "of", "in", "it", "on", "he", "as", "do", "at", "by", "we", "or", "an", "my", "so", "up", "if", "go", "me", "no", "us", "is", "am"])
 engWordList3 = frozenset(["and", "the", "for", "not", "you", "but", "his", "say", "her", "she", "one", "all", "out", "who", "get", "can", "him", "see", "now", "its", "use", "two", "how", "our", "way", "new", "any", "day", "are", "was"])
-engWordList4 = frozenset(["that", "have", "with", "this", "from", "they", "will", "what", "when", "make", "like", "time", "just", "know", "take", "into", "year", "your", "good", "some", "them", "work", "even", "want", "give", "most", "well", "than", "then", "look", "only", "come", "over", "also", "back",
-"would", "there", "their", "about", "which", "could", "other", "think", "after", "first", "these",
-"people", "because"])
+engWordList4 = frozenset(["that", "have", "with", "this", "from", "they", "will", "what", "when", "make", "like", "time", "just", "know", "take", "into", "year", "your", "good", "some", "them", "work", "even", "want", "give", "most", "well", "than", "then", "look", "only", "come", "over", "also", "back", "would", "there", "their", "about", "which", "could", "other", "think", "after", "first", "these", "people", "because"])
 
 #format the frequency table roughly with tabs
 #TODO:redo with https://docs.python.org/3.5/library/string.html#format-specification-mini-language
 head = "{a:s}\t{b:s}\t{c:s}\t{d:s}"
-head2 = "{a:s}\t{b:s}\t{c:s}\t{d:s}\t\t{e:s}\t{f:s}\t{f:s}"
+head2 = "{a:s}\t{b:s}\t{c:s}\t{d:s}\t\t{e:s}\t{f:s}\t{g:s}"
 fmt = "{a:s}\t{b:d}\t{c:0.2f}%\t{d:0.2f}%"
 fmt2 = "{a:s}\t{b:d}\t{c:0.2f}%\t{d:0.2f}%\t\t{e:s}\t{f:d}\t{g:b}"
 
@@ -101,40 +126,20 @@ print(head2.format(a = '', b = 'in', c = 'in', d = 'in', e = "Sequ-", f = 'in', 
 print(head2.format(a = '', b = 'text', c = 'text', d = 'English', e = 'ence', f = 'Text', g = 'English'))
 
 for letter in important_chars:
-    print(fmt2.format(a = letter, b = letter_freq[letter],
+	print(fmt2.format(a = letter, b = letter_freq[letter],
 	 c = (letter_freq[letter]/numLetters)*100,
 	 d = engLetterFreq[letter], e = letter+letter, f = bool(di_letter_freq[letter]),
- 	 g = bool(letter+letter in engDiLetterList))) #if english letters
+	 g = bool(letter+letter in engDiLetterList))) #if english letters
 
-#word frequency table
-#TODO: strip the '-' word
-unique_words = word_freq.most_common()		#turn the Counter into a list
-one_letter_word_freq = list()				#create separate lists for one,
-two_letter_word_freq = list()				#two and three letter words
-tri_letter_word_freq = list()				#and longer ones, but only if they
-long_word_freq = list()						#appear multiple times
-#<list>[x][0] gives the key, <list>[x][1] gives the number
-numWords = len(unique_words) #may containg junk such as -
-
-for i in unique_words:
-	#print(i[0], i[1])	#for debugging
-	if len(i[0]) == 1:
-		one_letter_word_freq.append(i)
-	elif len(i[0]) == 2:
-		two_letter_word_freq.append(i)
-	elif len(i[0]) == 3:
-		tri_letter_word_freq.append(i)
-	elif i[1] > 1:
-		long_word_freq.append(i)
 print()
 print(head.format(a = 'Word', b = 'Times', c = 'Freq', d = 'Freq'))
 print(head.format(a = '', b = 'in', c = 'in', d = 'in'))
 print(head.format(a = '', b = 'text', c = 'text', d = 'English'))
 for i in one_letter_word_freq:
-    print(fmt.format(a = i[0], b = i[1], c = ((i[1]/numWords)*100), d = 0))
+	print(fmt.format(a = i[0], b = i[1], c = ((i[1]/numWords)*100), d = 0))
 for i in two_letter_word_freq:
-    print(fmt.format(a = i[0], b = i[1], c = ((i[1]/numWords)*100), d = 0))
+	print(fmt.format(a = i[0], b = i[1], c = ((i[1]/numWords)*100), d = 0))
 for i in tri_letter_word_freq:
-    print(fmt.format(a = i[0], b = i[1], c = ((i[1]/numWords)*100), d = 0))
+	print(fmt.format(a = i[0], b = i[1], c = ((i[1]/numWords)*100), d = 0))
 for i in long_word_freq:
-    print(fmt.format(a = i[0], b = i[1], c = ((i[1]/numWords)*100), d = 0))
+	print(fmt.format(a = i[0], b = i[1], c = ((i[1]/numWords)*100), d = 0))
